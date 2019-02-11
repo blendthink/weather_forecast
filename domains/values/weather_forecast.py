d99745766961a7974c4cf16e355494fe406d5d98
Honwaka Developer <honwaka.developer@gmail.com>
2019-02-12 02:09:06 +0900

    気温情報についての処理をweather_forecastに追加

--- domains/values/weather_forecast.py
+++ domains/values/weather_forecast.py
@@ -10,3 +10,23 @@ class WeatherForecast:
     weather: str
     max_celsius: int or None
     min_celsius: int or None
+
+    def __get_min_celsius(self)->str:
+
+        if self.min_celsius is None:
+            return '最低気温情報なし'
+
+        return '最低気温は%s℃' % self.min_celsius
+
+    def __get_max_celsius(self)->str:
+
+        if self.max_celsius is None:
+            return '最高気温情報なし'
+
+        return '最高気温は%s℃' % self.max_celsius
+
+    def get_celsius(self)->str:
+        if self.max_celsius is self.min_celsius is None:
+            return '気温情報なし'
+
+        return '%s %s' % (self.__get_min_celsius(), self.__get_max_celsius())
